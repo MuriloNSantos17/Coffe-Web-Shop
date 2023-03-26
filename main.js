6cd5febad68a1ee228ff4760bf7f3d1a6dc84374
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2023-03-26 14:08:09 -0300

    commit final

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 let search = document.querySelector('.search-box');
-
 let searchIcon = document.querySelector('#search-icon');
 
-
 searchIcon.addEventListener('click',(evt)=>{
+    evt.preventDefault();
     search.classList.toggle('active');
+    navBar.classList.remove('active');
 });
+
+let navBar = document.querySelector('.navbar');
+let menuIcon = document.getElementById('menu-icon')
+menuIcon.addEventListener('click',(evt)=>{
+    evt.preventDefault();
+    navBar.classList.toggle('active');
+    search.classList.remove('active');
+})
+
+
+window.onscroll = ()=>{
+    navBar.classList.remove('active');
+    search.classList.remove('active');
+}
+
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', ()=>{
+    header.classList.toggle('shadow',window.scrollY >0);
+})
